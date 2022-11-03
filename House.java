c50b3202685294ccab20c2b29d8936f98b8f9a0c
vpliego <vpliego@smith.edu>
2022-11-02 20:07:31 -0400

    finalsub

--- House.java
+++ House.java
@@ -1,12 +1,43 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+public class House extends Building {
+  ArrayList<String> residents;
+  boolean hasDiningRoom;
+  int nResidents;
+  
+  public House(String name, String address, int nFloors, boolean hasDR) {
+    super(name, address, nFloors);
+    this.hasDiningRoom=hasDR;
+    this.residents=new ArrayList<String>();
+  }
+  
+    
+  public int nResidents(){
+    return this.nResidents;
+  }
+  public boolean hasDiningRoom(){
+    return this.hasDiningRoom;
+  }
+    
+  public void moveIn(String name){
+    this.residents.add(name);
+    this.nResidents += 1;
+  }
+    
+  public String moveOut(String name){
+    this.residents.remove(name);
+    this.nResidents -= 1;
+    return name;
+  }
+  
+  public boolean isResident(String person){
+    return this.residents.contains(person);
+    
   }
 
+  
   public static void main(String[] args) {
-    new House();
+    new House("Park Annex", "117 Elm St, Northampton, MA 01063", 3, true);
   }
 
 }
--- Library.java
+++ Library.java
@@ -1,7 +1,15 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.HashTable; 
 
-    public Library() {
+public class Library extends Building {
+
+private HashTable<String, Boolean> collection;
+
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new HashTable<String, Boolean>();
+
+
       System.out.println("You have built a library: 📖");
     }
   
